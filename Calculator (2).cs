4ded51e6773e62ebd9259215d53d1df909afe409
Haukka10 <86968870+Haukka10@users.noreply.github.com>
2022-05-31 20:50:53 +0200

    Add files via upload

---
+++ Calculator (2).cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shoop.Class
+{
+    public class ProductDescribe
+    {
+        public string NameProduct { get; set; }
+        public string DescribeProduct { get; set; }
+
+    }
+    public class Maths
+    {
+        public double VAT { get; set; }
+        public double output { get; set; }
+        public double total { get; set; }
+        public double Tax { get; set; }
+        
+    }
+}
